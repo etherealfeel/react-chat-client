845ff34039ea6ba0ea5c3324cb7c7e33d459f6b1
etherealfeel <graciagodsyndrome@gmail.com>
2023-01-17 23:06:00 +0300

    Add message state control

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:4000");
 
 const Chat = () => {
-  return (
-    <div>Chat</div>
-  )
-}
+  const { search } = useLocation();
+  const [state, setState] = useState([]);
+  const [params, setParams] = useState(null);
 
-export default Chat
+  useEffect(() => {
+    const searchParams = Object.fromEntries(new URLSearchParams(search));
+    setParams(searchParams);
+    socket.emit("join", searchParams);
+  }, [search]);
+
+  useEffect(() => {
+    socket.on("message", ({ data }) => {
+      setState((_state) => [..._state, data]);
+    });
+  }, []);
+
+  console.log(state);
+
+
+  return <div>Chat</div>;
+};
+
+export default Chat;
