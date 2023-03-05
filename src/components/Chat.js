fa248d59852c057e767ffd16f4ac8f758032fc87
etherealfeel <graciagodsyndrome@gmail.com>
2023-03-05 23:49:24 +0300

    Change server connection link

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,19 +1,19 @@
-import { useState, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import io from "socket.io-client";
-import EmojiPicker from "emoji-picker-react";
-import Messages from "./Messages";
-import MoodIcon from "@mui/icons-material/Mood";
-import { Container, Button, Box, Typography, Input } from "@mui/material";
-import SendIcon from "@mui/icons-material/Send";
+import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import io from 'socket.io-client';
+import EmojiPicker from 'emoji-picker-react';
+import Messages from './Messages';
+import MoodIcon from '@mui/icons-material/Mood';
+import { Container, Button, Box, Typography, Input } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
 
-const socket = io.connect(process.env.REACT_APP_HOST);
+const socket = io.connect('https://chat-server-ef.onrender.com');
 
 const Chat = () => {
   const { search } = useLocation();
   const [state, setState] = useState([]);
-  const [params, setParams] = useState({ username: "", room: "" });
-  const [message, setMessage] = useState("");
+  const [params, setParams] = useState({ username: '', room: '' });
+  const [message, setMessage] = useState('');
   const [isOpen, setOpen] = useState(false);
   const [users, setUsers] = useState(0);
   const navigate = useNavigate();
@@ -21,24 +21,24 @@ const Chat = () => {
   useEffect(() => {
     const searchParams = Object.fromEntries(new URLSearchParams(search));
     setParams(searchParams);
-    socket.emit("join", searchParams);
+    socket.emit('join', searchParams);
   }, [search]);
 
   useEffect(() => {
-    socket.on("getUsers", ({ data: { userCount } }) => {
+    socket.on('getUsers', ({ data: { userCount } }) => {
       setUsers(userCount);
     });
   }, []);
 
   useEffect(() => {
-    socket.on("message", ({ data }) => {
+    socket.on('message', ({ data }) => {
       setState((_state) => [..._state, data]);
     });
   }, []);
 
   const leftRoom = () => {
-    socket.emit("leftRoom", { params });
-    navigate("/");
+    socket.emit('leftRoom', { params });
+    navigate('/');
   };
 
   const handleChange = ({ target: { value } }) => {
@@ -49,8 +49,8 @@ const Chat = () => {
     e.preventDefault();
 
     if (!message) return;
-    socket.emit("sendMessage", { message, params });
-    setMessage("");
+    socket.emit('sendMessage', { message, params });
+    setMessage('');
   };
 
   const onEmojiClick = ({ emoji }) => setMessage(`${message} ${emoji}`);
@@ -59,35 +59,35 @@ const Chat = () => {
     <Container component="main" maxWidth="lg">
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          border: "2px solid violet",
-          backgroundColor: "white",
+          display: 'flex',
+          flexDirection: 'column',
+          border: '2px solid violet',
+          backgroundColor: 'white',
           borderRadius: 2,
-          height: "95vh",
-          marginTop: "10px",
+          height: '95vh',
+          marginTop: '10px',
         }}>
         <Box
           component="header"
           sx={{
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "center",
-            padding: "15px 0",
-            borderRadius: "20px 20px 0 0",
-            borderBottom: "2px solid violet",
-            backgroundColor: "#fff",
-            marginBottom: "10px"
+            display: 'flex',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            padding: '15px 0',
+            borderRadius: '20px 20px 0 0',
+            borderBottom: '2px solid violet',
+            backgroundColor: '#fff',
+            marginBottom: '10px',
           }}>
           <Typography
             component="h1"
             variant="h5"
             fontFamily="'Space Grotesk', sans-serif"
             sx={{
-              color: "#000",
-              borderRadius: "20px",
-              backgroundColor: "#eaeaea",
-              padding: "5px 15px",
+              color: '#000',
+              borderRadius: '20px',
+              backgroundColor: '#eaeaea',
+              padding: '5px 15px',
             }}>
             Room #{params.room}
           </Typography>
@@ -96,10 +96,10 @@ const Chat = () => {
             variant="h5"
             fontFamily="'Space Grotesk', sans-serif"
             sx={{
-              color: "#000",
-              borderRadius: "20px",
-              backgroundColor: "#eaeaea",
-              padding: "5px 15px",
+              color: '#000',
+              borderRadius: '20px',
+              backgroundColor: '#eaeaea',
+              padding: '5px 15px',
             }}>
             Users: {users}
           </Typography>
@@ -109,14 +109,14 @@ const Chat = () => {
         </Box>
         <Box
           sx={{
-            overflowY: "auto",
+            overflowY: 'auto',
             flexGrow: 1,
-            width: "100%",
-            color: "#fff",
-            fontSize: "18px",
-            background: "#fff",
-            padding: "0 32px",
-            "&::-webkit-scrollbar": { display: "none" },
+            width: '100%',
+            color: '#fff',
+            fontSize: '18px',
+            background: '#fff',
+            padding: '0 32px',
+            '&::-webkit-scrollbar': { display: 'none' },
           }}>
           <Messages messages={state} username={params.username} />
         </Box>
@@ -124,42 +124,31 @@ const Chat = () => {
           component="form"
           onSubmit={handleSubmit}
           sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            padding: "20px 30px",
-            margin: "10px 20px 20px 20px",
-            border: "2px solid violet"
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: '20px 30px',
+            margin: '10px 20px 20px 20px',
+            border: '2px solid violet',
           }}>
           <Box position="relative">
             <MoodIcon onClick={() => setOpen(!isOpen)} cursor="pointer" />
             {isOpen && (
               <Box
                 sx={{
-                  position: "absolute",
-                  left: "50%",
-                  transform: "translateX(-50%)",
-                  bottom: "120%",
-                  border: "1px solid black",
-                  borderRadius: "10px",
+                  position: 'absolute',
+                  left: '50%',
+                  transform: 'translateX(-50%)',
+                  bottom: '120%',
+                  border: '1px solid black',
+                  borderRadius: '10px',
                 }}>
                 <EmojiPicker onEmojiClick={onEmojiClick} />
               </Box>
             )}
           </Box>
-          <Input
-            value={message}
-            placeholder="Type something here"
-            color="secondary"
-            onChange={handleChange}
-            onClick={() => setOpen(false)}
-            sx={{ width: "80%" }}
-          />
-          <Button
-            onClick={handleSubmit}
-            variant="contained"
-            color="secondary"
-            endIcon={<SendIcon />}>
+          <Input value={message} placeholder="Type something here" color="secondary" onChange={handleChange} onClick={() => setOpen(false)} sx={{ width: '80%' }} />
+          <Button onClick={handleSubmit} variant="contained" color="secondary" endIcon={<SendIcon />}>
             Send
           </Button>
         </Box>
